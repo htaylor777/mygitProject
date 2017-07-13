b1d29fc698b3972e0dca8d428b7fd68b871207fd
ltaylor510 <ltaylor.jazz@gmail.com>
2017-07-12 22:20:24 -0700

    the first one

---
+++ ArrayListTest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class ArrayListTest{
+public static void main(String args[]){
+ArrayList<String> al = new ArrayList<String>();
+
+al.add("do");
+al.add("re");
+al.add("meena");
+al.add("fa");
+System.out.println("print all array:"+al);
+
+for(String a:al){
+System.out.println("print all with for(String:"+a);
+}
+for(int i =0; i < al.size();i++){
+System.out.println("ii++=:"+al.get(i)); 
+}
+System.out.println(al.get(2)); 
+}
+}
